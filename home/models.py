ae80f5f5b61246882e1f06eb2edee4a330a542f4
Omid Ahmadi <o.ahmadi1992@gmail.com>
2022-01-23 18:22:51 +0330

    Create Templates Link them Create Model Person

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    Phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    #gender = models.BooleanField()
+    #photo = models.ImageField(upload_to='home/Persons', null=True)
